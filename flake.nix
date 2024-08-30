a2374b3be46147cb472708e53c93e1f0bbb71b97
hugo-berendi <hugo.berendi@outlook.de>
2024-08-31 01:44:23 +0200

    fix!:

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "hugo-berendi's NixVim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixvim = {
       url = "github:nix-community/nixvim";
